33dd3d8770fe9196acaa95a7c8ba29affce422e2
Gaztambide <francogaztambide@gmail.com>
2016-02-17 01:13:27 -0500

    fixed landing page routing and image view

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-  
-  validates :name, presence: true
+
+  validates :title, presence: true
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pictures
 
-  # root 'landing#index'
-  # get 'landing' => 'landing#index'
+  root 'pictures#index'
+  get 'pictures' => 'pictures#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
